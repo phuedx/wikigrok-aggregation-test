f5b7eefd0d93b4d4cd5142561797d0b62dd0fc62
6ahodir <6ahodir@gmail.com>
2015-03-17 18:59:21 -0400

    Push data to wikidata

--- wikigrok-aggregation-test.py
+++ wikigrok-aggregation-test.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import csv
 import hashlib
 from time import strptime
@@ -5,11 +6,14 @@ import os
 
 import yaml
 
+from core import pwb, pywikibot
+
 
 class Aggregator():
     def __init__(self):
         self.config = {}
         self.load_config(os.path.join(os.path.curdir, 'config.yaml'))
+        self.claims = []
 
     def load_config(self, config_path):
         """Load config"""
@@ -19,7 +23,7 @@ class Aggregator():
     def aggregate(self):
         raw_claims = open('claims.tsv', 'r')
         claims = csv.DictReader(raw_claims, delimiter='\t')
-        aggregated_claims = {}
+        aggregated_claims = OrderedDict()
         cutoff_time = strptime('2014-12-12', '%Y-%m-%d')
 
         for claim in claims:
@@ -50,18 +54,64 @@ class Aggregator():
                 aggregated_claims[key]['positive_votes'] += 1
 
         for key, aggregated_claim in aggregated_claims.items():
-            if aggregated_claim['votes'] >= self.config['number_of_responses']:
-                print('-' * 10)
-                print('CLAIM: ' + str(aggregated_claim['claim']))
-                print('VOTES: ' + str(aggregated_claim['votes']))
-                print('POSITIVE_VOTES: ' + str(aggregated_claim['positive_votes']))
+            if aggregated_claim['votes'] >= self.config['number_of_responses'] and\
+                    float(aggregated_claim['positive_votes']) / aggregated_claim['votes'] >=\
+                    float(self.config['percent_agreement']) / 100:
+                self.claims.append(aggregated_claim)
+                # print('-' * 10)
+                # print('CLAIM: ' + str(aggregated_claim['claim']))
+                # print('VOTES: ' + str(aggregated_claim['votes']))
+                # print('POSITIVE_VOTES: ' + str(aggregated_claim['positive_votes']))
 
     def push(self):
-        # TODO: push data to wikidata
-        pass
+        pushed_claims = 0
+        if self.config['max_claims'] == -1:
+            max_claims = len(self.claims)
+        else:
+            max_claims = self.config['offset'] + self.config['max_claims']
 
+        site = pywikibot.Site('en', 'wikipedia')
+        repo = site.data_repository()
+        album_id = 'Q482994'  # album
+
+        for claim in self.claims[self.config['offset']:max_claims]:
+            subject_id = claim['claim']['event_subjectId']
+            property_id = claim['claim']['event_propertyId']
+            value_id = claim['claim']['event_valueId']
+
+            item = pywikibot.ItemPage(repo, subject_id)
+            item.get()
+
+            if item.claims:
+                claim_items = []
+                if property_id in item.claims:
+                    claim_items = [x.getTarget().title() for x in item.claims[property_id]]
+
+                # remove 'album' if we are adding 'studio album' or 'live album'
+                # 'Q208569' - studio album, 'Q209939' - live album
+                if property_id == 'P31' and\
+                        value_id in ('Q208569', 'Q209939') and album_id in claim_items:
+                    claim_to_remove = pywikibot.Claim(
+                        site, property_id, datatype='wikibase-item')
+                    claim_to_remove_item = pywikibot.ItemPage(repo, album_id)
+                    claim_to_remove.setTarget(claim_to_remove_item)
+                    # TODO: enable the next line if you want to remove 'album' claim
+                    # item.removeClaims([claim_to_remove])
+
+                if property_id not in item.claims or value_id not in claim_items:
+                    new_claim = pywikibot.Claim(
+                        site, claim['claim']['event_propertyId'], datatype='wikibase-item')
+                    new_claim_item = pywikibot.ItemPage(repo, claim['claim']['event_valueId'])
+                    new_claim.setTarget(new_claim_item)
+                    # TODO: enable the next line if you want to push data to wikidata
+                    # item.addClaim(new_claim)
+                    pushed_claims += 1
+
+        print('# of qualified claims: %d' % len(self.claims))
+        print('# of pushed claims: %d' % pushed_claims)
 
 if __name__ == '__main__':
-    ad = Aggregator()
-    ad.aggregate()
+    a = Aggregator()
+    a.aggregate()
+    a.push()
 
