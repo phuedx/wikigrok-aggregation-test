588dcfe2a283931d81b4ee9945ec064d39b82a44
Sam Smith <git@samsmith.io>
2015-03-19 11:34:40 +0000

    Remove {Claim,AggregatedClaim}.page_id attribute
    Also match the parameter orders of Claim and AggregatedClaim#__init__.

--- wikigrok/aggregated_claim.py
+++ wikigrok/aggregated_claim.py
@@ -2,8 +2,7 @@ from pipe import *
 
 
 class AggregatedClaim:
-    def __init__(self, page_id, subject_id, property_id, value_id, num_votes):
-        self.page_id = page_id
+    def __init__(self, subject_id, property_id, value_id, num_votes):
         self.subject_id = subject_id
         self.property_id = property_id
         self.value_id = value_id
@@ -19,7 +18,6 @@ def from_claims(claims):
     num_votes = claims_list | where(lambda c: c.is_positive) | count
 
     return AggregatedClaim(
-        claim.page_id,
         claim.subject_id,
         claim.property_id,
         claim.value_id,
--- wikigrok/claim.py
+++ wikigrok/claim.py
@@ -3,11 +3,10 @@ import time
 
 
 class Claim:
-    def __init__(self, page_id, property_id, subject_id, value_id, is_positive,
+    def __init__(self, subject_id, property_id, value_id, is_positive,
                  timestamp):
-        self.page_id = page_id
-        self.property_id = property_id
         self.subject_id = subject_id
+        self.property_id = property_id
         self.value_id = value_id
         self.is_positive = is_positive
         self.timestamp = timestamp
@@ -16,7 +15,6 @@ class Claim:
     def get_group_id(self):
         if self._group_id is None:
             hash_ = hashlib.md5()
-            hash_.update(self.page_id.encode('utf-8'))
             hash_.update(self.property_id.encode('utf-8'))
             hash_.update(self.subject_id.encode('utf-8'))
             hash_.update(self.value_id.encode('utf-8'))
@@ -34,9 +32,8 @@ def from_event(event):
         is_positive = False
 
     return Claim(
-        event['event_pageId'],
-        event['event_propertyId'],
         event['event_subjectId'],
+        event['event_propertyId'],
         event['event_valueId'],
         is_positive,
         time.strptime(event['timestamp'], '%Y%m%d%H%M%S')
